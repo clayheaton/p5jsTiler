edad3c26d7ea126ee44d502fe1986aec0c104307
clayheaton <ccheaton@gmail.com>
2016-03-06 21:41:52 -0500

    additional test images

--- js/p5js_setupExamples.js
+++ js/p5js_setupExamples.js
@@ -1,30 +1,56 @@
+var aggregateSet  = ['img/aggregate/aggregate1.jpg'];
 var crackedMudSet = ['img/crackedmud/crackedmud1.png',
                      'img/crackedmud/crackedmud2.png'];
 var grassSet      = ['img/grass/grass1.png',
                      'img/grass/grass2.png'];
+var grassDeadSet  = ['img/grassdead/grassdead1.jpg'];
+var grassDrySet   = ['img/grassdry/grassdry1.jpg'];
 var grassLongSet  = ['img/grasslong/grasslong1.png',
                      'img/grasslong/grasslong2.png'];
 var grassMondoSet = ['img/grassmondo/grassmondo1.png'];
 var gravelSet     = ['img/gravel/gravel1.png',
                      'img/gravel/gravel2.png',
                      'img/gravel/gravel3.png'];
+var leavesWeirdSet= ['img/leavesweird/leavesweird1.jpg'];
 var marsSet       = ['img/mars/mars1.png',
                      'img/mars/mars2.png'];
+var mossSet       = ['img/moss/moss1.jpg'];
 var muddyWaterSet = ['img/muddywater/muddywater1.png'];
+var mulchPineSet  = ['img/mulchpine/mulchpine1.jpg'];
+var mulchRedSet   = ['img/mulchred/mulchred1.jpg'];
 var pinkFlowerSet = ['img/pinkflowers/pinkflowers1.png'];
+var sidewalkSet   = ['img/sidewalk/sidewalk1.jpg',
+                     'img/sidewalk/sidewalk2.jpg',
+                     'img/sidewalk/sidewalk3.jpg'];
+var stoneRoughSet = ['img/stonerough/stonerough1.jpg'];
+var stoneSmoothSet= ['img/stonesmooth/stonesmooth1.jpg',
+                     'img/stonesmooth/stonesmooth2.jpg'];
+var stoneWallSet  = ['img/stonewall/stonewall1.jpg',
+                     'img/stonewall/stonewall2.jpg'];
 var waterSet      = ['img/water/water1.png'];
 var barkSet       = ['img/bark/bark1.png'];
 
 var exampleDict   = {
     'Select an Example Set' : null,
+    'Aggregate'    : aggregateSet,
     'Bark'         : barkSet,
     'Cracked Mud'  : crackedMudSet,
     'Grass'        : grassSet,
+    'Grass - Dead' : grassDeadSet,
+    'Grass - Dry'  : grassDrySet,
     'Grass - Long' : grassLongSet,
     'Grass - Mondo': grassMondoSet,
     'Gravel'       : gravelSet,
+    'Leaves - Weird':leavesWeirdSet,
     'Mars'         : marsSet,
+    'Moss'         : mossSet,
     'Muddy Water'  : muddyWaterSet,
+    'Mulch - Pine' : mulchPineSet,
+    'Mulch - Red'  : mulchRedSet,
     'Pink Flowers' : pinkFlowerSet,
+    'Sidewalk'     : sidewalkSet,
+    'Stone - Rough': stoneRoughSet,
+    'Stone - Smooth': stoneSmoothSet,
+    'Stone Wall'   : stoneWallSet,
     'Water'        : waterSet 
 };
